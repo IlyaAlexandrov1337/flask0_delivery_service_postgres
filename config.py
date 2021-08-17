44452662fdc79454b1e44cd7249f74f9f6072f7e
IlyaAlexandrov1337 <63933038+IlyaAlexandrov1337@users.noreply.github.com>
2021-08-17 14:11:11 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,8 @@ import os
 class Config:
     DEBUG = False
     SECRET_KEY = '1a0b329df51147t0va111335d2acbfd8'
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    uri = os.environ.get("DATABASE_URL")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    SQLALCHEMY_DATABASE_URI = uri
     SQLALCHEMY_TRACK_MODIFICATIONS = False
